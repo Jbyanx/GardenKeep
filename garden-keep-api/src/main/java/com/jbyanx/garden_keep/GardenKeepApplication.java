8ca198fba6104b128a928ce6515ff9064ee4daa0
jbyanx <108432086+Jbyanx@users.noreply.github.com>
2026-04-07 13:35:08 -0500

    chore(api): initial project structure from spring initializr

---
+++ garden-keep-api/src/main/java/com/jbyanx/garden_keep/GardenKeepApplication.java
@@ -0,0 +1,13 @@
+package com.jbyanx.garden_keep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenKeepApplication.class, args);
+	}
+
+}
---
+++ garden-keep-api/src/test/java/com/jbyanx/garden_keep/GardenKeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbyanx.garden_keep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenKeepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
