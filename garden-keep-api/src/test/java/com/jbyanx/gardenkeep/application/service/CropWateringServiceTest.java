ad152306af7f4a79398a1aeb3bce5975f8f4de43
jbyanx <108432086+Jbyanx@users.noreply.github.com>
2026-04-09 12:07:30 -0500

    test(application): add mockito scenario for domain rule violation

--- garden-keep-api/src/test/java/com/jbyanx/gardenkeep/application/service/CropWateringServiceTest.java
+++ garden-keep-api/src/test/java/com/jbyanx/gardenkeep/application/service/CropWateringServiceTest.java
@@ -30,14 +30,14 @@ class CropWateringServiceTest {
         //1. Arrange
         UUID cropId = UUID.randomUUID();
         //usamos una cebolla en fase 2 para que no lance excepcion
-        Crop onion = new Crop(cropId, CropType.ONION, GrowthStage.PHASE_2_DEEP);
+        Crop onion = new Crop(cropId, CropType.ONION, GrowthStage.PHASE_2_DEEP, null);
 
         //MOCKITO IN ACTION
         // Le decimos al mock: "Cuando alguien te pida buscar este ID, devuelve esta cebolla".
         when(cropRepository.findById(cropId)).thenReturn(Optional.of(onion));
 
         //2.Act se registra el riego profundo
-        cropWateringService.recordDeepWatering(cropId);
+        cropWateringService.waterCrop(cropId, true);
 
         //3. asserts verificamos que el repositorio falso
         //llama al metodo save una vez
@@ -53,11 +53,29 @@ class CropWateringServiceTest {
         when(cropRepository.findById(fakeId)).thenReturn(Optional.empty());
 
         //act y assert
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> cropWateringService.recordDeepWatering(fakeId));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> cropWateringService.waterCrop(fakeId, true));
 
         assertEquals("El cultivo con ID " + fakeId + " no existe.", exception.getMessage());
 
         //verificamos que nunca se haya intentado guardar nada en BD
+        verify(cropRepository, never()).save(any());
+    }
+    @Test
+    void shouldNotSaveWhenDomainRuleIsViolated(){
+        UUID cropId = UUID.randomUUID();
+        Crop onion = new Crop(cropId, CropType.ONION, GrowthStage.PHASE_1_SURFACE, null);
+
+        when(cropRepository.findById(cropId)).thenReturn(Optional.of(onion));
+
+        // 2. Act & Assert: Intentamos regar con tierra HÚMEDA (false)
+        // El servicio debería propagar la excepción que lanza la entidad Crop
+        assertThrows(IllegalStateException.class, () -> {
+            cropWateringService.waterCrop(cropId, false);
+        });
+
+        // 3. Verificación de Seguridad
+        // ¡SUPER IMPORTANTE! Verificamos que NUNCA se llamó al save
+        // porque la regla botánica detuvo el proceso.
         verify(cropRepository, never()).save(any());
     }
 }
