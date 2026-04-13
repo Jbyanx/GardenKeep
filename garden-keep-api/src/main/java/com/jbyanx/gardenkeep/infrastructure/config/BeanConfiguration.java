9bd3bfdddbf60412f60ce9b9c6246a388c985902
jbyanx <108432086+Jbyanx@users.noreply.github.com>
2026-04-13 18:00:12 -0500

    config(infrastructure): update BeanConfiguration to use Postgres adapter with Qualifier

--- garden-keep-api/src/main/java/com/jbyanx/gardenkeep/infrastructure/config/BeanConfiguration.java
+++ garden-keep-api/src/main/java/com/jbyanx/gardenkeep/infrastructure/config/BeanConfiguration.java
@@ -2,16 +2,18 @@ package com.jbyanx.gardenkeep.infrastructure.config;
 
 import com.jbyanx.gardenkeep.application.port.out.CropRepositoryPort;
 import com.jbyanx.gardenkeep.application.service.CropWateringService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfiguration {
 
+    //aca decidimos que adaptador de base de datos usamos, en este caso el de postgres
     @Bean
-    public CropWateringService cropWateringService(CropRepositoryPort cropRepositoryPort) {
+    public CropWateringService cropWateringService(@Qualifier("postgresCropRepositoryAdapter") CropRepositoryPort cropRepositoryPort) {
         // Aquí "inyectamos" manualmente las dependencias.
-        // Spring buscará una implementación de CropRepositoryPort (que haremos luego).
+        // Spring buscará una implementación de CropRepositoryPort.
         return new CropWateringService(cropRepositoryPort);
     }
 }
