595f98707aad144419a67426fad9025926a3da1c
jbyanx <108432086+Jbyanx@users.noreply.github.com>
2026-04-14 23:50:54 -0500

    feat(domain): add DomainException.java to establish a hierarchy with domain exceptions

---
+++ garden-keep-api/src/main/java/com/jbyanx/gardenkeep/domain/exception/DomainException.java
@@ -0,0 +1,7 @@
+package com.jbyanx.gardenkeep.domain.exception;
+
+public abstract class DomainException extends RuntimeException {
+    protected DomainException(String message) {
+        super(message);
+    }
+}
